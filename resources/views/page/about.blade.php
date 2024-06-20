cecb3ba8e56d64b2d4c204717ac7485ea1ff4095
phanduyhao <haomrvuii@gmail.com>
2024-06-21 01:18:49 +0700

    commit

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,7 +1,129 @@
 @extends('layouts.layout')
 @section('content')
-    <section class="about-us section container mt-5">
-        <h1 class="mt-5"> Chào mừng đến với cửa hàng máy tính Văn Hiệp - Computer</h1>
+<div class="breadcrumbs">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6 col-12">
+                <div class="breadcrumbs-content">
+                    <h1 class="page-title">About Us</h1>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12">
+                <ul class="breadcrumb-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li>About Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
-    </section>
+<section class="about-us section">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6 col-md-12 col-12">
+                <div class="content-left wow fadeInLeft" data-wow-delay=".3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                    <img src="assets/images/about/choose-left.jpg" alt="#">
+                    <a href="https://www.youtube.com/watch?v=r44RKWyfcFw&amp;fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM" class="glightbox video"><i class="lni lni-play"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 col-12">
+                <!-- content-1 start -->
+                <div class="content-right wow fadeInRight" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInRight;">
+                    <!-- Heading -->
+                    <span class="sub-heading">About</span>
+                    <h2>
+                        About Our Company</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi aliquip ex ea commodo consequat.</p>
+                    <h3>What We Do</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt
+                        ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    <!-- End Heading -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="client-logo-section">
+    <div class="container">
+        <div class="client-logo-wrapper">
+            <div class="tns-outer" id="tns1-ow"><div class="tns-liveregion tns-visually-hidden" aria-live="polite" aria-atomic="true">slide <span class="current">15 to 20</span>  of 9</div><div id="tns1-mw" class="tns-ovh"><div class="tns-inner" id="tns1-iw"><div class="client-logo-carousel d-flex align-items-center justify-content-between  tns-slider tns-carousel tns-subpixel tns-calc tns-horizontal" id="tns1" style="transform: translate3d(-45.1613%, 0px, 0px); transition-duration: 0s;"><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client1.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item" id="tns1-item0" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client1.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item" id="tns1-item1" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item" id="tns1-item2" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item3">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item4">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item5">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item6">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item7">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div>
+                <div class="client-logo tns-item tns-slide-active" id="tns1-item8">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div>
+            <div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client1.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client3.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client4.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client5.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client1.svg" alt="#">
+                </div><div class="client-logo tns-item tns-slide-cloned" aria-hidden="true" tabindex="-1">
+                    <img src="assets/images/clients/client2.svg" alt="#">
+                </div></div></div></div></div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -8,4 +8,111 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.1103481701143!2d105.69317477496648!3d18.659043582461578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3139cddf0bf20f23%3A0x86154b56a284fa6d!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBWaW5o!5e0!3m2!1svi!2s!4v1715878729145!5m2!1svi!2s" class="w-100" height="700" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
         </div>
     </div>
+    <section id="contact-us" class="contact-us section">
+        <div class="container">
+            <div class="contact-head wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                <div class="row">
+                    <div class="col-lg-5 col-12">
+                        <div class="single-head">
+                            <div class="contant-inner-title">
+                                <h2>Our Contacts &amp; Location</h2>
+                                <p>Business consulting excepteur sint occaecat cupidatat consulting non proident.</p>
+                            </div>
+                            <div class="single-info">
+                                <h3>Opening hours</h3>
+                                <ul>
+                                    <li>Daily: 9.30 AM–6.00 PM</li>
+                                    <li>Sunday &amp; Holidays: Closed</li>
+                                </ul>
+                            </div>
+                            <div class="single-info">
+                                <h3>Contact info</h3>
+                                <ul>
+                                    <li>77408 Satterfield Motorway Suite</li>
+                                    <li>469 New Antonetta, BC K3L6P6</li>
+                                    <li><a href="mailto:info@yourwebsite.com">example@info.com</a></li>
+                                    <li><a href="tel:(617) 495-9400-326">(617) 495-9400-326</a></li>
+                                </ul>
+                            </div>
+                            <div class="single-info contact-social">
+                                <h3>Social contact</h3>
+                                <ul>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-facebook-original"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-twitter-original"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="lni lni-pinterest"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-12">
+                        <div class="form-main">
+                            <div class="form-title">
+                                <h2>Get in Touch</h2>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have
+                                    suffered alteration in some form.</p>
+                            </div>
+                            <form class="form" method="post" action="assets/mail/mail.php">
+                                <div class="row">
+                                    <div class="col-lg-6 col-12">
+                                        <div class="form-group">
+                                            <input name="name" type="text" placeholder="Your Name" required="required">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-12">
+                                        <div class="form-group">
+                                            <input name="subject" type="text" placeholder="Your Subject" required="required">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-12">
+                                        <div class="form-group">
+                                            <input name="email" type="email" placeholder="Your Email" required="required">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-12">
+                                        <div class="form-group">
+                                            <input name="phone" type="text" placeholder="Your Phone" required="required">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-group message">
+                                            <textarea name="message" placeholder="Your Message"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-group button">
+                                            <button type="submit" class="btn ">Submit Message</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="map-section">
+        <div class="map-container">
+            <div class="mapouter">
+                <div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=New%20York&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.com">123movies old site</a>
+                    <style>
+                        .mapouter {
+                            position: relative;
+                            text-align: right;
+                            height: 500px;
+                            width: 100%;
+                        }
+
+                        .gmap_canvas {
+                            overflow: hidden;
+                            background: none !important;
+                            height: 500px;
+                            width: 100%;
+                        }
+                    </style>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
